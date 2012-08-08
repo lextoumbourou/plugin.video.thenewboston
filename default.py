9bfa15d0c3bd6c9be1852c85a73803e81656e439
Lex Toumbourou <lextoumbourou@gmail.com>
2012-08-08 19:46:06 +1000

    Refactored code to fit PEP8

--- default.py
+++ default.py
@@ -4,18 +4,14 @@ from urllib import quote, unquote
 import xbmcaddon
 from resources.lib import scraper, utils
 
-### get addon info
-__addon__             = xbmcaddon.Addon()
-__addonid__           = __addon__.getAddonInfo('id')
-__addonidint__        = int(sys.argv[1])
-
 def main(params):
     if not params.has_key('mode') or params['mode'] == 'list_categories':
         html = scraper.open_page('http://thenewboston.org/tutorials.php')
         categories = scraper.get_categories(html)
+        logo = 'http://thenewboston.org/images/theNewBoston_logo.png'
         for category in categories:
             utils.add_directory_link(category['title'], 
-                                     'http://thenewboston.org/images/theNewBoston_logo.png', 
+                                     logo, 
                                      'list_topics', 
                                      category['title'], 
                                      is_folder=True, 
@@ -26,9 +22,10 @@ def main(params):
         title = params['url']
         html = scraper.open_page('http://thenewboston.org/tutorials.php')
         topics = scraper.get_topics(html, title)
+        logo = 'http://thenewboston.org/images/theNewBoston_logo.png'
         for topic in topics:
             utils.add_directory_link(topic['title'], 
-                                     'http://thenewboston.org/images/theNewBoston_logo.png', 
+                                     logo, 
                                      'list_lessons', 
                                      quote(topic['url']), 
                                      is_folder=True, 
@@ -39,9 +36,10 @@ def main(params):
         url = unquote(params['url'])
         html = scraper.open_page(url)
         lessons = scraper.get_lessons(html)
+        logo = 'http://thenewboston.org/images/theNewBoston_logo.png'
         for lesson in lessons:
             utils.add_directory_link(lesson['title'], 
-                                     'http://thenewboston.org/images/theNewBoston_logo.png', 
+                                     logo, 
                                      'play_video', 
                                      quote(lesson['url']), 
                                      is_folder=False, 
@@ -52,7 +50,8 @@ def main(params):
     elif params['mode'] == 'play_video':
         html = scraper.open_page(unquote(params['url']))
         youtube_url, youtube_id = scraper.get_youtube(html)
-        url = "plugin://plugin.video.youtube/?path=/root/video&action=play_video&videoid={0}".format(youtube_id)
+        url = ("plugin://plugin.video.youtube/?path=/root/video"
+               "&action=play_video&videoid={0}").format(youtube_id)
         utils.play_video(url)
 
     utils.end_directory()
